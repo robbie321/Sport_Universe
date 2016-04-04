d60888bc09ca8dfd741eb3f9973c486a2486394b
MALONE ROBERT <MALONE ROBERT>
2016-04-04 19:59:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/g13149881/asportuniverse/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.g13149881.asportuniverse;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton soccer = (ImageButton) findViewById(R.id.soccer_button);
+        Button ufc = (Button) findViewById(R.id.ufc_button);
+        ImageButton rugby = (ImageButton) findViewById(R.id.rugby_button);
+
+
+        soccer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Loading Soccer data", Toast.LENGTH_SHORT).show();
+                Intent myIntent = new Intent(MainActivity.this, SoccerRss.class);
+                startActivity(myIntent);
+
+
+            }
+        });
+        ufc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Loading UFC data", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        rugby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Loading Rugby data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/g13149881/asportuniverse/SoccerRss.java
@@ -0,0 +1,35 @@
+package com.example.g13149881.asportuniverse;
+
+/**
+ * Created by g13149881 on 04/04/2016.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SoccerRss extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soccer_rss);
+
+
+        Button home = (Button) findViewById(R.id.button_home);
+
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Home selected", Toast.LENGTH_SHORT).show();
+                Intent myIntent = new Intent(SoccerRss.this, MainActivity.class);
+                SoccerRss.this.startActivity(myIntent);
+
+
+            }
+        });
+    }
+}
