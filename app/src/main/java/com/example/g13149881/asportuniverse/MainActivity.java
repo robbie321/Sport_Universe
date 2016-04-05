36110bbc8b103d8cf467816012697bd0fa207a9b
Robbie Malone <robmalone7@gmail.com>
2016-04-05 17:42:06 +0100

    updated xml files with ListView's

--- app/src/main/java/com/example/g13149881/asportuniverse/MainActivity.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
