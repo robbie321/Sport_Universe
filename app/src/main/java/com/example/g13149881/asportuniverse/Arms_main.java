f9d9d5556d16b7592a41c7a8516969a0059a7742
Robbie Malone <robmalone7@gmail.com>
2016-04-22 01:16:09 +0100

    created .onCreates

--- app/src/main/java/com/example/g13149881/asportuniverse/Arms_main.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Arms_main.java
@@ -1,10 +1,16 @@
 package com.example.g13149881.asportuniverse;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by mblackweir on 21/04/2016.
  */
 public class Arms_main extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arms_layout);
+    }
 
 }
--- app/src/main/java/com/example/g13149881/asportuniverse/Legs_main.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Legs_main.java
@@ -1,9 +1,15 @@
 package com.example.g13149881.asportuniverse;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by mblackweir on 21/04/2016.
  */
 public class Legs_main extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.legs_layout);
+    }
 }
