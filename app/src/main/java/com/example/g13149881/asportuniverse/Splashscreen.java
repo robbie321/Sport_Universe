01fe2f59402841bc62289cc3a204970ff94b628b
darran-blacky <darran.blacky@gmail.com>
2016-04-23 22:49:55 +0100

    Edited splashscreen,mainActivity & exercise xml files

--- app/src/main/java/com/example/g13149881/asportuniverse/Splashscreen.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Splashscreen.java
@@ -2,64 +2,33 @@ package com.example.g13149881.asportuniverse;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.os.Handler;
 
 /**
  * Created by mblackweir on 08/04/2016.
  */
 
 public class Splashscreen extends Activity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
-    public static final int seconds = 4;
-    public static final int miliseconds = seconds * 1000;
-
-    private ProgressBar progressBar;
-
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.splashscreen_layout);
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(seconds - 1);
-        progressBar.getProgressDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
-
-        startAnimation();
-    }
-
-    public void startAnimation() {
-        new CountDownTimer(miliseconds, 1000) {
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
             @Override
-            public void onTick(long millisUntilFinished) {
-                //progressbar update--->1 second
-                progressBar.setProgress(getMiliseconds(millisUntilFinished));
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splashscreen.this,MainActivity.class);
+                Splashscreen.this.startActivity(mainIntent);
+                finish();
             }
-
-            @Override
-            public void onFinish() {
-                Intent i = new Intent(Splashscreen.this, MainActivity.class);
-                Splashscreen.this.startActivityForResult(i, 1);
-            }
-        }.start();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //when I return to the MainActivity, finish my app
-        if (requestCode == 1) finish();
-    }
-
-    public int getMiliseconds(long milis) {
-        return (int) ((miliseconds - milis) / 1000);
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
