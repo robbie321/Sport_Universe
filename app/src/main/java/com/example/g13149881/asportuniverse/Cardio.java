eedd89411d4e7ba429228eeaaf99c562330e15b5
darran-blacky <darran.blacky@gmail.com>
2016-04-24 17:53:27 +0100

    put a video view in the cadio file and added permissions to the manifest

--- app/src/main/java/com/example/g13149881/asportuniverse/Cardio.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Cardio.java
@@ -2,9 +2,12 @@ package com.example.g13149881.asportuniverse;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 /**
  * Created by g13155105 on 04/04/2016.
@@ -15,5 +18,14 @@ public class Cardio extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cardio_layout);
+
+        VideoView videoView =(VideoView)findViewById(R.id.videoView);
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri= Uri.parse("rtsp://r7---sn-4g57kue6.googlevideo.com/Ck0LENy73wIaRAmk3cJBg-iaXhMYDSANFC3u0pRWMOCoAUIJbXYtZ29vZ2xlSARSBXdhdGNoYIaluaTkzciOVooBCzVxRjNraG5XcXdnDA==/D693A8E7577C3A29E60C292B42C9C87D7C25A565.762A63DC4CA0A028DA83256C6A79E5F160CBEDA3/yt6/1/video.3gp");
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
     }
 }
