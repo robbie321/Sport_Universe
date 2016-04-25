2283703dfc68556b60615e78c820170e2f5b8c7f
Robbie <robmalone7@gmail.com>
2016-04-25 22:56:23 +0100

    7 DAY FOOD PLAN

--- app/src/main/java/com/example/g13149881/asportuniverse/Lean.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Lean.java
@@ -10,6 +10,6 @@ public class Lean extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lean_layout);
+        setContentView(R.layout.svendaymeal_layout);
     }
 }
