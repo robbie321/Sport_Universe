65d225745dafc56559b36125e21a1daceba5d6c0
Darran Blackweir <darran.blacky@gmail.com>
2016-04-25 21:02:02 +0100

    Merge branch 'master' of C:\Users\g13155105\AndroidStudioProjects\Sport_Universe with conflicts.

---
+++ app/src/main/java/com/example/g13149881/asportuniverse/Bulk.java
@@ -0,0 +1,15 @@
+package com.example.g13149881.asportuniverse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by g13155105 on 25/04/2016.
+ */
+public class Bulk extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bulk_layout);
+    }
+}
--- app/src/main/java/com/example/g13149881/asportuniverse/Nutrition.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Nutrition.java
@@ -11,6 +11,33 @@ public class Nutrition extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nutrition);
+        Button Exercise = (Button) findViewById(R.id.exercise_button);
+        Button Nutrition = (Button) findViewById(R.id.nutrition_button);
+        Button Cardio = (Button) findViewById(R.id.cardio_button);
+
+        Exercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(Nutrition.this, Bulk.class);
+                startActivity(myIntent);
+            }
+        });
+        Nutrition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+        Cardio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(Nutrition.this, Cardio.class);
+                startActivity(myIntent);
+            }
+        });
 
     }
 }
