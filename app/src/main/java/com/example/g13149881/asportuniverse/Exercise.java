6359876755677d7f5136f27cae665260b8f1c9b1
Robbie Malone <robmalone7@gmail.com>
2016-05-01 11:16:26 +0100

    added comments

--- app/src/main/java/com/example/g13149881/asportuniverse/Exercise.java
+++ app/src/main/java/com/example/g13149881/asportuniverse/Exercise.java
@@ -16,7 +16,7 @@ public class Exercise extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise_layout);
 
-
+        //creating buttons for exercise activity
         Button chest = (Button) findViewById(R.id.chest_button);
         Button back = (Button) findViewById(R.id.back_button);
         Button shoulders = (Button) findViewById(R.id.shoulders_button);
@@ -24,7 +24,7 @@ public class Exercise extends Activity
         Button legs = (Button) findViewById(R.id.legs_button);
         Button abs = (Button) findViewById(R.id.abs_button);
 
-
+        // listening for chest button click
         chest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,37 +32,57 @@ public class Exercise extends Activity
                 startActivity(myIntent);
             }
         });
+            // listening for back button click
             back.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //intent constructor takes a Context as it's first parameter
+                    // (.this is used because Exercise is a subclass of context)
+                    //and then the class of the page to where the system delivers the Intent
                     Intent myIntent = new Intent(Exercise.this, Back_main.class);
                     startActivity(myIntent);
                 }
             });
+            // listening for shoulders button click
             shoulders.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //intent constructor takes a Context as it's first parameter
+                    // (.this is used because Exercise is a subclass of context)
+                    //and then the class of the page to where the system delivers the Intent
                     Intent myIntent = new Intent(Exercise.this, Shoulders_main.class);
                     startActivity(myIntent);
                 }
             });
+            // listening for arms button click
             arms.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //intent constructor takes a Context as it's first parameter
+                    // (.this is used because Exercise is a subclass of context)
+                    //and then the class of the page to where the system delivers the Intent
                     Intent myIntent = new Intent(Exercise.this, Arms_main.class);
                     startActivity(myIntent);
                 }
             });
+            // listening for leg button click
             legs.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //intent constructor takes a Context as it's first parameter
+                    // (.this is used because Exercise is a subclass of context)
+                    //and then the class of the page to where the system delivers the Intent
                     Intent myIntent = new Intent(Exercise.this, Legs_main.class);
                     startActivity(myIntent);
                 }
             });
+            // listening for abs button click
             abs.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //intent constructor takes a Context as it's first parameter
+                    // (.this is used because Exercise is a subclass of context)
+                    //and then the class of the page to where the system delivers the Intent
                     Intent myIntent = new Intent(Exercise.this, Abs_main.class);
                     startActivity(myIntent);
                 }
